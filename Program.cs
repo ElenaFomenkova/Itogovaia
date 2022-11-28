a77f2a4cdb35d21b73ee119b6572de5891649159
Elena <Fomenkova.len@ya.ru>
2022-11-29 00:10:30 +0300

    код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fillArray(string inStr)
+{
+    return inStr.Split(" ");
+}
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+Console.Write("Введите слова через пробел");
+
+string str = Convert.ToString(Console.ReadLine());
+
+string[] array = fillArray(str);
+printArray(array);
